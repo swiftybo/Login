04b3469d5d59f242c342e47e6a59ee8ca5e04ee2
swiftybo <leungjordan28@gmail.com>
2025-11-26 18:58:39 +0000

    added icons for login fields and component for altenativeLogin. Completed styling for LoginFrom section in css modules. Moved toggle to MainLogin page.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -29,78 +29,60 @@ function logInAction(prevFormState, formData) {
 export default function LoginForm() {
   // const loginCtx = useContext(LoginContext);
   const [formState, formAction] = useActionState(logInAction, { errors: null });
-  console.log(formState);
 
   return (
     <>
-      <form action={formAction}>
-        <div>
-          <label htmlFor="username">Username or Email</label>
+      <form action={formAction} className={classes.loginForm}>
+        <div className={classes.loginForm__field}>
+          <label htmlFor="username"></label>
           <input
+            className={classes.loginForm__input}
             id="username"
             type="text"
             name="username"
             placeholder="Username or Email"
             defaultValue={formState.enteredDetails?.username}
-            required
           ></input>
         </div>
 
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className={classes.loginForm__field}>
+          <label htmlFor="password"></label>
           <input
+            className={classes.loginForm__input}
             id="password"
             type="password"
             name="password"
             placeholder="Password"
-            required
           ></input>
         </div>
 
-        <div>
-          <label htmlFor="remember">
-            Remember me
+        <section className={classes.loginForm__toolsSection}>
+          <div className={classes.loginForm__checkbox}>
+            <label htmlFor="remember">Remember me </label>
             <input
+              style={{ "-webkit-transform": "scale(1.25)" }}
               id="remember"
               type="checkbox"
               name="remember"
               //BUG: checkbox seems to be always checked by default (apart from first render)
               defaultChecked={formState.enteredDetails?.remember}
             />
-          </label>
-        </div>
+          </div>
 
-        <div>
-          <Link>Forgot Password?</Link>
-        </div>
-
-        <label className={classes.switch}>
-          <span className={classes.sun}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <g fill="#ffd43b">
-                <circle r="5" cy="12" cx="12"></circle>
-                <path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path>
-              </g>
-            </svg>
-          </span>
-          <span className={classes.moon}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-              <path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path>
-            </svg>
-          </span>
-          <input type="checkbox" className={classes.input} />
-          <span className={classes.slider}></span>
-        </label>
-
-        <button>Log In</button>
+          <div>
+            <Link>Forgot Password?</Link>
+          </div>
+        </section>
 
         {formState.errors && (
-          <ul>
+          <ul className={classes.loginForm__errorsList}>
             {formState.errors.map((error) => (
               <li key={error}>{error}</li>
             ))}
           </ul>
         )}
+
+        <button className={classes.loginForm__button}>Log In</button>
       </form>
     </>
   );
